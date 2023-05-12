e5dab512360ab04845a602f594d0c0ddf28fba4f
isidorakaran <karanisidora@hotmail.com>
2023-05-12 19:53:08 +0200

    Creating employee

---
+++ PastryShopFDP/src/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+    private PastryShop cheesecake;
+    private PastryShop muffin;
+    private PastryShop pudding;
+    public Employee{
+        cheesecake=new Cheesecake();
+        muffin=new Muffin();
+        pudding=new Pudding();
+    }
+
+    public void cheesecakeSale{
+        cheesecake.name();
+        cheesecake.price();
+    }
+
+    public void muffinSale{
+        muffin.name();
+        muffin.price();
+    }
+
+    public void puddingSale{
+        pudding.name();
+        pudding.price();
+    }
+    
+}
