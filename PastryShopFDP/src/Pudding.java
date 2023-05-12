000d192faa661c0d780dea359b923d08978fce03
isidorakaran <karanisidora@hotmail.com>
2023-05-12 19:48:33 +0200

    Creating pudding

---
+++ PastryShopFDP/src/Pudding.java
@@ -0,0 +1,12 @@
+public class Pudding implements PastryShop{
+
+    @Override
+    public void name() {
+        System.out.println("Pudding");
+    }
+
+    @Override
+    public void price() {
+        System.out.println("2$");
+    }
+}
