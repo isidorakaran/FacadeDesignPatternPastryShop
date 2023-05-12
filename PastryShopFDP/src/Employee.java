ba8ee3457bc2e0bae22bca0c509782f0ae2b77d2
isidorakaran <karanisidora@hotmail.com>
2023-05-12 20:02:08 +0200

    Finished mini project

--- PastryShopFDP/src/Employee.java
+++ PastryShopFDP/src/Employee.java
@@ -2,25 +2,25 @@ public class Employee {
     private PastryShop cheesecake;
     private PastryShop muffin;
     private PastryShop pudding;
-    public Employee{
+    public Employee() {
         cheesecake=new Cheesecake();
         muffin=new Muffin();
         pudding=new Pudding();
     }
 
-    public void cheesecakeSale{
+    public void cheesecakeSale(){
         cheesecake.name();
         cheesecake.price();
     }
 
-    public void muffinSale{
+    public void muffinSale(){
         muffin.name();
         muffin.price();
     }
 
-    public void puddingSale{
+    public void puddingSale(){
         pudding.name();
         pudding.price();
     }
-    
+
 }
---
+++ PastryShopFDP/src/FacadePatternClient.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FacadePatternClient {
+    private static int  choice;
+    public static void main(String args[]) throws NumberFormatException, IOException{
+        do{
+            System.out.print("*=========* Mobile Shop *============* \n");
+            System.out.print("            1. Cheesecake.              \n");
+            System.out.print("            2. Muffin.              \n");
+            System.out.print("            3. Pudding.            \n");
+            System.out.print("            4. Exit.                     \n");
+            System.out.print("Please enter your choice: ");
+
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            choice=Integer.parseInt(br.readLine());
+            Employee e=new Employee();
+
+            switch (choice) {
+                case 1:
+                {
+                    e.cheesecakeSale();
+                }
+                break;
+                case 2:
+                {
+                    e.muffinSale();
+                }
+                break;
+                case 3:
+                {
+                    e.puddingSale();
+                }
+                break;
+                default:
+                {
+                    System.out.println("Goodbye! :)");
+                }
+                return;
+            }
+
+        }while(choice!=4);
+    }
+}
+
